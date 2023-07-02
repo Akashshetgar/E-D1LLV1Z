9199c8446012b4add49177f43651c03b84b09c5f
Akashshetgar <akash.shetgar@somaiya.edu>
2023-07-03 01:14:25 +0530

    feat: emit json

--- server/index.js
+++ server/index.js
@@ -22,10 +22,12 @@ const tcpClient = new net.Socket();
 
 dotenv.config();
 
+const jsonPkts = [];
+
 tcpClient.connect(8000, "localhost", () => {
   console.log("Connected to TCP server.");
   tcpClient.write(Buffer.from([0x41]));
-
+  const jsonPkts = [];
   // Event handler for receiving data from TCP server
   tcpClient.on("data", (data) => {
     
@@ -34,6 +36,13 @@ tcpClient.connect(8000, "localhost", () => {
     var i = 0;
     while(i<data.length-130) {
       const packetLength = data.subarray(i, i+4);
+      if(packetLength.readInt32LE(0) !== 124) {
+        console.log("packet length is not correct: ",packetLength.readInt32LE(), " ",packetLength.readInt16LE());
+        // console.log("smaller packet ",packetLength.subarray(0,2).readInt32LE(), " ",packetLength.subarray(0,2).readInt16LE());
+        i = i + packetLength.readInt32LE(0);
+        // setTimeout(() => {}, 1000);
+        continue;
+      }
       const tradingSymbol = data.subarray(i+4, i+34);
       const sequnceNumber = data.subarray(i+34, i+42);
       const timeStamp = data.subarray(i+42, i+50);
@@ -48,8 +57,8 @@ tcpClient.connect(8000, "localhost", () => {
       const OI = data.subarray(i+106, i+114);
       const prevClosePrice = data.subarray(i+114, i+122);
       const prevCloseInterest = data.subarray(i+122, i+130);
-      
-      console.log({
+
+      const jsonPkt = {
         "packetLength": packetLength.readInt32LE(0),
         "tradingSymbol": tradingSymbol.toString('utf8'),
         "sequnceNumber":Number(sequnceNumber.readBigInt64LE()),
@@ -65,6 +74,25 @@ tcpClient.connect(8000, "localhost", () => {
         "prevClosePrice":Number( prevClosePrice.readBigInt64LE()),
         "prevCloseInterest": Number(prevCloseInterest.readBigInt64LE())
   
+      };
+      jsonPkts.push(jsonPkt);
+      
+      console.log({
+        packetLength : packetLength.readInt32LE(0),
+        tradingSymbol: tradingSymbol.toString('utf8'),
+        sequnceNumber:Number(sequnceNumber.readBigInt64LE()),
+        timeStamp: date.toISOString(),
+        LTP:Number( LTP.readBigInt64LE()),
+        LTQ:Number( LTQ.readBigInt64LE()),
+        volume:Number( volume.readBigInt64LE()),
+        bidPrice: Number(bidPrice.readBigInt64LE()),
+        bidQty: Number(bidQty.readBigInt64LE()),
+        askPrice:Number( askPrice.readBigInt64LE()),
+        askQuantity:Number( askQuantity.readBigInt64LE()),
+        OI:Number( OI.readBigInt64LE()),
+        prevClosePrice:Number( prevClosePrice.readBigInt64LE()),
+        prevCloseInterest: Number(prevCloseInterest.readBigInt64LE())
+  
       });
       i = i + 130;
     }
@@ -77,11 +105,13 @@ tcpClient.connect(8000, "localhost", () => {
   tcpClient.on("close", () => {
     console.log("TCP connection closed.");
   });
-});
+  });
 
 socket_io_server.on('connection', (socket) => {
   console.log("client connected");
-  socket.emit("data", { message: "Hello from server!" });
+
+  socket.emit("data", jsonPkts);
+
     socket.on("disconnect", () => {
       console.log("Socket.io client disconnected.");
     });

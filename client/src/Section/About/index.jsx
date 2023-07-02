2e16be6ac19cbb66db96a71b69b2ab78dd5c6474
Dwayne232001 <vaznigeldwayne@gmail.com>
2023-07-02 23:26:36 +0530

    Added Scrollable Table

--- client/src/Section/About/index.jsx
+++ client/src/Section/About/index.jsx
@@ -1,26 +1,102 @@
 import Heading from "../../Components/Heading";
 import styles from "./styles.module.css";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { TableVirtuoso } from 'react-virtuoso';
+
+
+const sample = [
+	['Frozen yoghurt', 159, 6.0, 24, 4.0],
+	['Ice cream sandwich', 237, 9.0, 37, 4.3],
+	['Eclair', 262, 16.0, 24, 6.0],
+	['Cupcake', 305, 3.7, 67, 4.3],
+	['Gingerbread', 356, 16.0, 49, 3.9],
+];
+
+function createData(id, dessert, calories, fat, carbs, protein) {
+return { id, dessert, calories, fat, carbs, protein };
+}
+
+const columns = [
+	{width: 200,label: 'Dessert',dataKey: 'dessert',},
+	{width: 120,label: 'Calories\u00A0(g)',dataKey: 'calories',numeric: true,},
+	{width: 120,label: 'Fat\u00A0(g)',dataKey: 'fat',numeric: true,},
+	{width: 120,label: 'Carbs\u00A0(g)',dataKey: 'carbs',numeric: true,},
+	{width: 120,label: 'Protein\u00A0(g)',dataKey: 'protein',numeric: true,},
+];
+  
+  const rows = Array.from({ length: 200 }, (_, index) => {
+	const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+	return createData(index, ...randomSelection);
+  });
+  
+  const VirtuosoTableComponents = {
+	Scroller: React.forwardRef((props, ref) => (
+	  <TableContainer component={Paper} {...props} ref={ref} />
+	)),
+	Table: (props) => (
+	  <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
+	),
+	TableHead,
+	TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
+	TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
+  };
+
+function fixedHeaderContent() {
+  return (
+    <TableRow>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          variant="head"
+          align={column.numeric || false ? 'right' : 'left'}
+          style={{ width: column.width,  }}
+          sx={{
+            backgroundColor: 'grey.300',
+          }}
+        >
+          {column.label}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
+
+function rowContent(_index, row) {
+  return (
+    <React.Fragment>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          align={column.numeric || false ? 'right' : 'left'}
+        >
+          {row[column.dataKey]}
+        </TableCell>
+      ))}
+    </React.Fragment>
+  );
+}
 
 function About() {
 	return (
 		<section id="about">
 			<Heading index="01" heading="About Me" />
-			<div className={styles.container}>
-				<div className={styles.left}>
-					<p className={styles.desc}>
-					table or data we could put here 
-					</p>
-					
-				</div>
-				<div className={styles.right}>
-					<img
-						src="/swarangi.jpg"
-						alt="profile"
-						className={styles.profile_img}
+			{/* <div className={styles.tableContainer}> */}
+				<Paper style={{ height: 400, width: '100%' }}>
+					<TableVirtuoso
+						data={rows}
+						components={VirtuosoTableComponents}
+						fixedHeaderContent={fixedHeaderContent}
+						itemContent={rowContent}
 					/>
-					<div className={styles.img_border}></div>
-				</div>
-			</div>
+				</Paper>
+			{/* </div> */}
 		</section>
 	);
 }
